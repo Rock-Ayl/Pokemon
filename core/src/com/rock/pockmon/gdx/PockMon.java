289866f267c6aace6d1ddc4d2b99e3971951d439
Rock-Ayl <AnYongLiang4869@163.com>
2022-10-17 22:08:42 +0800

    修改：识图了解资源管理器是什么个东西

--- core/src/com/rock/pockmon/gdx/PockMon.java
+++ core/src/com/rock/pockmon/gdx/PockMon.java
@@ -1,8 +1,10 @@
 package com.rock.pockmon.gdx;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.rock.pockmon.gdx.enums.PersonEnum;
 import com.rock.pockmon.gdx.model.people.Person;
 import com.rock.pockmon.gdx.screen.MainMenu;
@@ -21,8 +23,12 @@ public class PockMon extends Game {
 
     //绘制图形
     private SpriteBatch batch;
+
     //字体
     private BitmapFont font;
+
+    //资源管理器
+    private AssetManager assetManager;
 
     /**
      * 主角
@@ -42,8 +48,16 @@ public class PockMon extends Game {
 
         //初始化一个SpriteBatch,游戏中只有一个,直到游戏结束后销毁
         this.batch = new SpriteBatch();
+
         //使用默认的字体
         this.font = new BitmapFont();
+
+        //初始化资源管理器
+        this.assetManager = new AssetManager();
+        //加载已打包的图片资源
+        this.assetManager.load("assets/packed/image/people/ruby/walk/textures.atlas", TextureAtlas.class);
+        //加载资源完成
+        this.assetManager.finishLoading();
 
         /**
          * 主角
@@ -71,6 +85,7 @@ public class PockMon extends Game {
     public void dispose() {
         this.batch.dispose();
         this.font.dispose();
+        this.assetManager.dispose();
     }
 
     /**
@@ -87,6 +102,10 @@ public class PockMon extends Game {
 
     public Person getAdventurer() {
         return adventurer;
+    }
+
+    public AssetManager getAssetManager() {
+        return assetManager;
     }
 
 }

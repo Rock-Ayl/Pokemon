ac8e1c12aa693e90e61937e156cb811ecda0f2d3
Rock-Ayl <AnYongLiang4869@163.com>
2022-10-29 10:05:11 +0800

    修改：制作世界-34-优化了按键输入器,不再硬编码

--- core/src/com/rock/pokemon/gdx/controller/InputController.java
+++ core/src/com/rock/pokemon/gdx/controller/InputController.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.InputAdapter;
 import com.rock.pokemon.gdx.enums.DirectionEnum;
 import com.rock.pokemon.gdx.model.people.Person;
 
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
 /**
  * 输入控制器,根据输入处理移动等逻辑
  *
@@ -21,17 +24,22 @@ public class InputController extends InputAdapter {
     private Person person;
 
     /**
-     * 输入状态,状态可以全部存在,比如按上的同时也可以按下,但是怎么处理就我们说的算了
+     * 输入
      */
 
-    //代表当前方向的输入状态
-    private boolean up, down, left, right;
-
-    //每种方向的按键持续时间
-    private float upTime, downTime, leftTime, rightTime;
-
+    //输入状态,状态可以全部存在,比如按上的同时也可以按下,但是怎么处理就我们说的算了(目前30够用了)
+    private boolean[] buttonPress = new boolean[30];
+    //输入状态的持续时间(目前30够用了)
+    private float[] buttonTimeArr = new float[30];
     //方向按键持续时间不移动,方向键按住的时间小于等于该时间,则换方向不移动,大于该时间,开始移动判定
     private float notMoveTime = 0.15F;
+    //方向键编码列表初始化,判定也按照这个顺序来,主角移动判定,同一次移动只能一个方向,但输入状态可以有多个,按照绿宝石的手感判定,上优先级最高,下其次,左第三,右的判定最低
+    private static final LinkedHashSet<Integer> dirInputKeyLinkedSet = new LinkedHashSet<>(Arrays.asList(
+            Input.Keys.UP,
+            Input.Keys.DOWN,
+            Input.Keys.LEFT,
+            Input.Keys.RIGHT
+    ));
 
     /**
      * 初始化,指定要监听的人物
@@ -51,32 +59,12 @@ public class InputController extends InputAdapter {
      */
     @Override
     public boolean keyDown(int keycode) {
-        //根据输入决定方向状态
-        switch (keycode) {
-            case Input.Keys.UP:
-                //方向状态开启
-                up = true;
-                //重置其持续时间
-                this.upTime = 0F;
-                break;
-            case Input.Keys.DOWN:
-                //方向状态开启
-                down = true;
-                //重置其持续时间
-                this.downTime = 0F;
-                break;
-            case Input.Keys.LEFT:
-                //方向状态开启
-                left = true;
-                //重置其持续时间
-                this.leftTime = 0F;
-                break;
-            case Input.Keys.RIGHT:
-                //方向状态开启
-                right = true;
-                //重置其持续时间
-                this.rightTime = 0F;
-                break;
+        //如果按下方向键
+        if (dirInputKeyLinkedSet.contains(keycode)) {
+            //更改键位状态
+            buttonPress[keycode] = true;
+            //重置其持续时间
+            buttonTimeArr[keycode] = 0;
         }
         //默认返回
         return false;
@@ -90,52 +78,17 @@ public class InputController extends InputAdapter {
      */
     @Override
     public boolean keyUp(int keycode) {
-        //根据释放按键释放方向状态
-        switch (keycode) {
-            case Input.Keys.UP:
-                //如果按键持续时间特别短
-                if (upTime <= notMoveTime) {
-                    //单纯的脸换个方向
-                    this.person.changeDir(DirectionEnum.NORTH);
-                }
-                //关闭按键状态
-                up = false;
-                //重置持续时间
-                upTime = 0;
-                break;
-            case Input.Keys.DOWN:
-                //如果按键持续时间特别短
-                if (downTime <= notMoveTime) {
-                    //单纯的脸换个方向
-                    this.person.changeDir(DirectionEnum.SOUTH);
-                }
-                //关闭按键状态
-                down = false;
-                //重置持续时间
-                downTime = 0;
-                break;
-            case Input.Keys.LEFT:
-                //如果按键持续时间特别短
-                if (leftTime <= notMoveTime) {
-                    //单纯的脸换个方向
-                    this.person.changeDir(DirectionEnum.WEST);
-                }
-                //关闭按键状态
-                left = false;
-                //重置持续时间
-                leftTime = 0;
-                break;
-            case Input.Keys.RIGHT:
-                //如果按键持续时间特别短
-                if (rightTime <= notMoveTime) {
-                    //单纯的脸换个方向
-                    this.person.changeDir(DirectionEnum.EAST);
-                }
-                //关闭按键状态
-                right = false;
-                //重置持续时间
-                rightTime = 0;
-                break;
+        //如果释放方向键
+        if (dirInputKeyLinkedSet.contains(keycode)) {
+            //如果按键持续时间特别短
+            if (buttonTimeArr[keycode] <= notMoveTime) {
+                //尝试给脸还成对应方向
+                this.person.changeDir(DirectionEnum.parseByKeycode(keycode));
+            }
+            //关闭按键状态
+            buttonPress[keycode] = false;
+            //重置持续时间
+            buttonTimeArr[keycode] = 0;
         }
         //默认返回
         return false;
@@ -148,43 +101,22 @@ public class InputController extends InputAdapter {
      */
     public void update(float delta) {
 
-        /**
-         * 判定主角的移动
-         * 主角移动判定,同一次移动只能一个方向,但输入状态可以有多个,按照绿宝石的手感判定,上优先级最高,下其次,左第三,右的判定最低
-         */
-
-        if (up) {
-            //叠加其持续时间
-            upTime += delta;
-            //如果持续按
-            if (upTime > notMoveTime) {
-                //尝试移动判定
-                this.person.move(DirectionEnum.NORTH);
+        //循环方向键判定
+        for (Integer dirInputKey : dirInputKeyLinkedSet) {
+            //如果该方向按键没有被按着
+            if (buttonPress[dirInputKey] == false) {
+                //本轮过
+                continue;
             }
-        } else if (down) {
             //叠加其持续时间
-            downTime += delta;
-            //如果持续按
-            if (downTime > notMoveTime) {
-                //尝试移动判定
-                this.person.move(DirectionEnum.SOUTH);
+            buttonTimeArr[dirInputKey] += delta;
+            //如果没有持续按
+            if (buttonTimeArr[dirInputKey] <= notMoveTime) {
+                //本轮过
+                continue;
             }
-        } else if (left) {
-            //叠加其持续时间
-            leftTime += delta;
-            //如果持续按
-            if (leftTime > notMoveTime) {
-                //尝试移动判定
-                this.person.move(DirectionEnum.WEST);
-            }
-        } else if (right) {
-            //叠加其持续时间
-            rightTime += delta;
-            //如果持续按
-            if (rightTime > notMoveTime) {
-                //尝试移动判定
-                this.person.move(DirectionEnum.EAST);
-            }
+            //尝试移动判定
+            this.person.move(DirectionEnum.parseByKeycode(dirInputKey));
         }
 
     }
--- core/src/com/rock/pokemon/gdx/enums/DirectionEnum.java
+++ core/src/com/rock/pokemon/gdx/enums/DirectionEnum.java
@@ -1,30 +1,55 @@
 package com.rock.pokemon.gdx.enums;
 
+import com.badlogic.gdx.Input;
+
 /**
  * 方向枚举
  */
 public enum DirectionEnum {
 
-    NORTH(0, 1, "north", "北"),
-    EAST(1, 0, "east", "东"),
-    SOUTH(0, -1, "south", "南"),
-    WEST(-1, 0, "west", "西"),
+    NORTH(0, 1, Input.Keys.UP, "north", "北"),
+    EAST(1, 0, Input.Keys.RIGHT, "east", "东"),
+    SOUTH(0, -1, Input.Keys.DOWN, "south", "南"),
+    WEST(-1, 0, Input.Keys.LEFT, "west", "西"),
 
     ;
 
     //该方向每次移动坐标
     private int dx, dy;
 
+    //该方向对应的输入按键编码(暂时用枚举)
+    private int keycode;
+
     //名字
     private String name;
     //中文名
     private String zhName;
 
-    DirectionEnum(int dx, int dy, String name, String zhName) {
+    DirectionEnum(int dx, int dy, int keycode, String name, String zhName) {
         this.dx = dx;
         this.dy = dy;
+        this.keycode = keycode;
         this.name = name;
         this.zhName = zhName;
+    }
+
+    /**
+     * 根据方向输入,解析方向枚举
+     *
+     * @param keycode 案件编码
+     * @return
+     */
+    public static DirectionEnum parseByKeycode(int keycode) {
+        //循环
+        for (DirectionEnum value : DirectionEnum.values()) {
+            //如果对应上
+            if (value.keycode == keycode) {
+                //返回
+                return value;
+            }
+        }
+        //默认,南
+        return SOUTH;
     }
 
     /**

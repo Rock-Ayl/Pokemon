56afa60473b0d6def0c049d4f4c0e5370faa7b38
Rock-Ayl <AnYongLiang4869@163.com>
2022-11-06 07:54:15 +0800

    修改：UI-47-版本+1、readMe

--- core/src/com/rock/pokemon/gdx/common/Settings.java
+++ core/src/com/rock/pokemon/gdx/common/Settings.java
@@ -17,7 +17,7 @@ public class Settings {
     //标题
     public static final String TITLE = "Pokemon By Rock";
     //游戏版本
-    public static final String VERSION = "0.0.6";
+    public static final String VERSION = "0.0.7";
 
     //todo 当前语言(暂时固定为中文)
     public static final LanguageEnum LANG = LanguageEnum.SIMPLIFIED_CHINESE;

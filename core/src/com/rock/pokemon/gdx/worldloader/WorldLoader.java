a60c55d016bdef71e3c5ab4090417bbb4e6dae5d
Rock-Ayl <AnYongLiang4869@163.com>
2023-09-14 09:14:02 +0800

    修改：世界载入器及配置-4

--- core/src/com/rock/pokemon/gdx/worldloader/WorldLoader.java
+++ core/src/com/rock/pokemon/gdx/worldloader/WorldLoader.java
@@ -1,7 +1,6 @@
 package com.rock.pokemon.gdx.worldloader;
 
 import com.alibaba.fastjson.JSON;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.assets.AssetLoaderParameters;
 import com.badlogic.gdx.assets.AssetManager;
@@ -11,10 +10,6 @@ import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Array;
 import com.rock.pokemon.gdx.model.map.World;
 import com.rock.pokemon.gdx.model.mapConfig.MapConfig;
-import org.apache.commons.io.FileUtils;
-
-import java.io.IOException;
-import java.nio.charset.Charset;
 
 /**
  * 加载世界对象
@@ -30,19 +25,8 @@ public class WorldLoader extends AsynchronousAssetLoader<World, WorldLoader.Worl
 
     @Override
     public void loadAsync(AssetManager assetManager, String filename, FileHandle file, WorldParameter parameter) {
-        //配置内容
-        String fileStr = "";
-        try {
-            //读取配置文件
-            fileStr = FileUtils.readFileToString(file.file(), Charset.defaultCharset());
-        } catch (IOException e) {
-            //日志
-            Gdx.app.error(this.getClass().getName(), "读取配置文件", e);
-        }
-        //解析为对应实体
-        MapConfig mapConfig = JSON.parseObject(fileStr, MapConfig.class);
-        //初始化世界
-        this.world = new World(assetManager, mapConfig);
+        //读取配置文件、解析为对应配置实体、初始化世界
+        this.world = new World(assetManager, JSON.parseObject(file.readString(), MapConfig.class));
     }
 
     @Override

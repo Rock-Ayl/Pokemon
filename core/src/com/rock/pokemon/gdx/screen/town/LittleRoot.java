c039445abaafd9e561ca963c9605b6a8e2fa07ee
Rock-Ayl <AnYongLiang4869@163.com>
2023-11-21 23:28:27 +0800

    修改：对话框控制器-3

--- core/src/com/rock/pokemon/gdx/screen/town/LittleRoot.java
+++ core/src/com/rock/pokemon/gdx/screen/town/LittleRoot.java
@@ -14,7 +14,6 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.rock.pokemon.gdx.Pokemon;
 import com.rock.pokemon.gdx.common.FilePaths;
 import com.rock.pokemon.gdx.common.Settings;
-import com.rock.pokemon.gdx.controller.DialogueBoxController;
 import com.rock.pokemon.gdx.controller.OptionBoxController;
 import com.rock.pokemon.gdx.controller.PersonController;
 import com.rock.pokemon.gdx.model.map.World;
@@ -69,9 +68,6 @@ public class LittleRoot implements Screen {
 
     //可选项框 控制器
     private OptionBoxController optionBoxController;
-
-    //对话框 控制器
-    private DialogueBoxController dialogueBoxController;
 
     /**
      * 世界
@@ -206,15 +202,12 @@ public class LittleRoot implements Screen {
 
         //初始化可选项框控制器
         this.optionBoxController = new OptionBoxController(this.optionBox);
-        //初始化对话框控制器
-        this.dialogueBoxController = new DialogueBoxController(this.dialogueBox);
         //初始化输入监听,控制主角的行动
         this.personController = new PersonController(this.adventurer);
 
         //组装至所有控制器
         this.inputMultiplexer.addProcessor(this.personController);
         this.inputMultiplexer.addProcessor(this.optionBoxController);
-        this.inputMultiplexer.addProcessor(this.dialogueBoxController);
 
     }
 
@@ -251,6 +244,16 @@ public class LittleRoot implements Screen {
         this.viewport.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         //渲染时使用相机
         this.game.getBatch().setProjectionMatrix(this.viewport.getCamera().combined);
+
+        /**
+         * 对话框操作
+         */
+
+        //如果按回车 and 对话完毕
+        if (Gdx.input.isKeyPressed(Input.Keys.ENTER) && this.dialogueBox.isFinished()) {
+            //对话框不可见
+            this.dialogueBox.setVisible(false);
+        }
 
         /**
          * 渲染世界及更新

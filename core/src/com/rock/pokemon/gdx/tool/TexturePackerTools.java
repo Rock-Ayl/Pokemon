13c368f2014e561649de9fadf1efeb18ac2e974d
Rock-Ayl <AnYongLiang4869@163.com>
2024-03-07 13:47:40 +0800

    修改：打包优化-4

--- core/src/com/rock/pokemon/gdx/tool/TexturePackerTools.java
+++ core/src/com/rock/pokemon/gdx/tool/TexturePackerTools.java
@@ -16,30 +16,27 @@ import java.util.stream.Collectors;
  */
 public class TexturePackerTools {
 
-    //未打包文件路径
-    private static String INPUT = "assets/unpacked/";
-
-    //已打包文件路径
-    private static String OUTPUT = "assets/packed/";
-
     /**
-     * 将图片、动画帧打包至对应目录
+     * 统一将图片、动画帧打包至对应目录
      *
      * @param args
      */
     public static void main(String[] args) {
-
+        //未打包文件路径
+        String input = "assets/unpacked/";
+        //已打包文件路径
+        String output = "assets/packed/";
         //收集并过滤文件,按照文件夹分组
-        Map<String, List<String>> pathGroupMap = FileExtraUtils.collectFile(INPUT)
+        Map<String, List<String>> pathGroupMap = FileExtraUtils.collectFile(input)
                 .stream()
                 //目前只有png的素材需要打包
-                .filter(p -> FilenameUtils.getExtension(p).equals("png"))
+                .filter(p -> "png".equalsIgnoreCase(FilenameUtils.getExtension(p)))
                 //按照文件夹分组
                 .collect(Collectors.groupingBy(FilenameUtils::getPath));
         //循环所有文件夹
         for (String inputPath : pathGroupMap.keySet()) {
             //生成对应打包路径
-            String outputPath = inputPath.replaceFirst(INPUT, OUTPUT);
+            String outputPath = inputPath.replaceFirst(input, output);
             //打包该路径下所有资源
             TexturePacker.process(inputPath, outputPath, "textures");
         }

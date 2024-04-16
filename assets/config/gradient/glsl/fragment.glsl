e7362b7a3bc75c455d48ea5832c2540df5e50396
Rock-Ayl <AnYongLiang4869@163.com>
2024-04-16 11:43:40 +0800

    修改：渐变效果-2

--- assets/config/gradient/glsl/fragment.glsl
+++ assets/config/gradient/glsl/fragment.glsl
@@ -8,10 +8,16 @@ uniform sampler2D u_texture;
 uniform float u_time;
 
 void main() {
+
     vec4 texColor = texture2D(u_texture, v_texCoords);
+
     //计算像素的亮度
     float brightness = dot(texColor.rgb, vec3(0.299, 0.587, 0.114));
-    //根据亮度和时间参数决定是否显示该像素
-    float alpha = step(u_time, brightness);
+
+    //为了在动画开始之前让所有像素保持不可见，我们需要适当调整亮度值和u_time的比较方式
+    //这样在u_time小于任何亮度值之前，所有像素都将不可见,确保亮度低于u_time的像素被显示
+    float alpha = step(brightness, u_time);
+
     gl_FragColor = vec4(texColor.rgb, texColor.a * alpha);
+
 }

38d9a07939bd8e45384bc3cae43b028fe1dfeb7f
Rock-Ayl <AnYongLiang4869@163.com>
2024-04-20 13:37:34 +0800

    修改：事件模块-13

---
+++ core/src/com/rock/pokemon/gdx/model/event/node/NpcHideEventNode.java
@@ -0,0 +1,27 @@
+package com.rock.pokemon.gdx.model.event.node;
+
+import com.rock.pokemon.gdx.enums.EventNodeTypeEnum;
+import com.rock.pokemon.gdx.model.event.EventNodeTemplate;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 人物隐藏 事件节点
+ *
+ * @Author ayl
+ * @Date 2024-04-20
+ */
+@Getter
+@Setter
+public class NpcHideEventNode implements EventNodeTemplate {
+
+    //对应npcId
+    private Integer npcId;
+
+    @Override
+    public EventNodeTypeEnum getType() {
+        //对应枚举类型
+        return EventNodeTypeEnum.NPC_HIDE;
+    }
+
+}

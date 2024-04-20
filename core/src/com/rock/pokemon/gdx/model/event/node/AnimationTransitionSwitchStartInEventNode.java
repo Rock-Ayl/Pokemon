1a603a9607e25c7225e1d530c955c409fc6a0faf
Rock-Ayl <AnYongLiang4869@163.com>
2024-04-20 14:26:05 +0800

    修改：事件模块-23

---
+++ core/src/com/rock/pokemon/gdx/model/event/node/AnimationTransitionSwitchStartInEventNode.java
@@ -0,0 +1,24 @@
+package com.rock.pokemon.gdx.model.event.node;
+
+import com.rock.pokemon.gdx.enums.EventNodeTypeEnum;
+import com.rock.pokemon.gdx.model.event.EventNodeTemplate;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 场景切换-淡入 事件节点
+ *
+ * @Author ayl
+ * @Date 2024-04-20
+ */
+@Getter
+@Setter
+public class AnimationTransitionSwitchStartInEventNode implements EventNodeTemplate {
+
+    @Override
+    public EventNodeTypeEnum getType() {
+        //对应枚举类型
+        return EventNodeTypeEnum.ANIMATION_TRANSITION_SWITCH_START_IN;
+    }
+
+}

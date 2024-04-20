ec070164b62d70cf225ad385880a7e302053347a
Rock-Ayl <AnYongLiang4869@163.com>
2024-04-20 14:12:49 +0800

    修改：事件模块-20

---
+++ core/src/com/rock/pokemon/gdx/model/event/node/WorldObjectShowEventNode.java
@@ -0,0 +1,27 @@
+package com.rock.pokemon.gdx.model.event.node;
+
+import com.rock.pokemon.gdx.enums.EventNodeTypeEnum;
+import com.rock.pokemon.gdx.model.event.EventNodeTemplate;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 事物显示 事件节点
+ *
+ * @Author ayl
+ * @Date 2024-04-20
+ */
+@Getter
+@Setter
+public class WorldObjectShowEventNode implements EventNodeTemplate {
+
+    //对应事物Id
+    private Integer worldObjectId;
+
+    @Override
+    public EventNodeTypeEnum getType() {
+        //对应枚举类型
+        return EventNodeTypeEnum.WORLD_OBJECT_SHOW;
+    }
+
+}

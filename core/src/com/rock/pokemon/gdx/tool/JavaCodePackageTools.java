55a3dca4131dc1d4fe494e7e80f28824c9283e9e
Rock-Ayl <AnYongLiang4869@163.com>
2025-02-05 17:04:17 +0800

    修改：事件模块-101

--- core/src/com/rock/pokemon/gdx/tool/JavaCodePackageTools.java
+++ core/src/com/rock/pokemon/gdx/tool/JavaCodePackageTools.java
@@ -1,7 +1,9 @@
 package com.rock.pokemon.gdx.tool;
 
 import com.rock.pokemon.gdx.util.FileExtraUtils;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,11 +16,11 @@ import java.util.stream.Collectors;
 public class JavaCodePackageTools {
 
     //路径
-    private static final String PATH = "/Users/ayl/work/Pokemon/core";
+    private static final String PATH = "/Users/ayl/work/Pokemon";
     //输出后的文件
     private static final String OUT_FILE = "/Users/ayl/Downloads/out.java";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         /**
          * step 1. 收集所有 .java 文件
@@ -31,17 +33,31 @@ public class JavaCodePackageTools {
                 .stream()
                 //仅保留 .java 后缀的
                 .filter(p -> p.endsWith(".java"))
+                //排序
+                .sorted()
                 .collect(Collectors.toList());
 
         /**
          * step 2. 读取所有内容并拼接
          */
 
-        //todo
+        //内容
+        StringBuilder str = new StringBuilder();
+        //循环
+        for (String javaFilePath : javaFilePathList) {
+            //解析文件内容
+            String fileContent = FileUtils.readFileToString(new File(javaFilePath), "UTF-8");
+            //组装、换行
+            str.append(fileContent).append("\n");
+        }
 
         /**
          * step 3. 输出到文件
          */
+
+        //写入文件
+        FileUtils.writeStringToFile(new File(OUT_FILE), str.toString());
+        System.out.println("===写入成功===");
 
     }
 

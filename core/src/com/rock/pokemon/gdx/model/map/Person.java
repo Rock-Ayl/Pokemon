9336460a6d9ec555784c8158e2bd3551f2830bb6
Rock-Ayl <AnYongLiang4869@163.com>
2026-03-30 17:31:46 +0800

    修改：场景移动-5

--- core/src/com/rock/pokemon/gdx/model/map/Person.java
+++ core/src/com/rock/pokemon/gdx/model/map/Person.java
@@ -9,6 +9,7 @@ import com.rock.pokemon.gdx.model.map.config.NpcMapConfig.NpcMapNode;
 import com.rock.pokemon.gdx.model.map.config.NpcMapConfig.NpcMapNodeEvent;
 import com.rock.pokemon.gdx.model.map.renderer.YSortable;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
@@ -33,6 +34,10 @@ public class Person implements YSortable {
 
     //备注
     private String remark;
+
+    //是否可见
+    @Setter
+    private boolean visible = true;
 
     //人物宽高,设定用户宽高,绿宝石中,通常人物高度占接近1.5个地图网格
     private float width = 1.0F;
